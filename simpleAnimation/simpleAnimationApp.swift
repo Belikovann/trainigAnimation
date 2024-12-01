706d7a5c7f098143562015a2a43a457f08658cbc
@Annabel_ <hydrant94@gmail.com>
2024-12-01 15:24:57 +0200

    Initial Commit

---
+++ simpleAnimation/simpleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAnimationApp.swift
+//  simpleAnimation
+//
+//  Created by Аня Беликова on 01.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct simpleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
