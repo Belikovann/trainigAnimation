c24ff195e84bb7501fa4a0b55020bb989ff6216a
@Annabel_ <hydrant94@gmail.com>
2024-12-01 16:45:22 +0200

    chore: core Animation for navigation

---
+++ simpleAnimation/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  simpleAnimation
+//
+//  Created by Аня Беликова on 01.12.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ simpleAnimation/TailView.swift
@@ -0,0 +1,18 @@
+//
+//  TailView.swift
+//  simpleAnimation
+//
+//  Created by Аня Беликова on 01.12.2024.
+//
+
+import SwiftUI
+
+struct TailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TailView()
+}
